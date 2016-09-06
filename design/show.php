3fcbd90cd7f2d5837fabe3e6666da2ca88ceb78e
yuyasss <soccer.kumakuma@gmail.com>
2016-09-06 16:47:32 +0900

    ヘッダーリンクの修正

--- design/show.php
+++ design/show.php
@@ -1,32 +1,33 @@
-<?php 
-//ここにDBに登録する処理を記述する。
-//１．DBへ接続
+
+
+<?php //INSERT INTO `friends`(`freind_id`, `friend_name`, `area_id`, `gender`, `age`, `created`, `modified`) VALUES ("","1","1","3","now()")
+
+
+//ここにDBに登録する処理を記述する
+//1.DBへ接続
   $dsn = 'mysql:dbname=myfriends;host=localhost';
   $user = 'root';
   $password = '';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->query('SET NAMES utf8');
-//2.SQLの作成
-$sql='SELECT * FROM `areas`';
-//3.SQL実行
+
+//２.GETパラメータを取得
+$area_id=$_Get['area_id'];
+
+//３．SQL文を作成
+$sql='SELECT `area_name` FROM `areas` WHERE `area_id`=?';
+$data[]=$area_id;
+
+//４.SQLを実行
 $stmt=$dbh->prepare($sql);
-$stmt->execute();
+$stmt->execute($data);
 
-//データ格納用変数
-$areas=array();
-
-//４．データ取得
-while (1) {
-  $rec=$stmt->fetch(PDO::FETCH_ASSOC);
-  if($rec==false){
-    break;
-  }
-  $areas[]=$rec;
-}
+//５.都道府県名を取得
+$area=$stmt->fetch(POD::FETCH_ASSOC);
 
 //DB切断
 $dbh=null;
-var_dump($areas);
+
 
  ?>
 <!DOCTYPE html>
@@ -39,11 +40,11 @@ var_dump($areas);
     <title>myFriends</title>
 
     <!-- Bootstrap -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="assets/css/form.css" rel="stylesheet">
-    <link href="assets/css/timeline.css" rel="stylesheet">
-    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../assets/css/form.css" rel="stylesheet">
+    <link href="../assets/css/timeline.css" rel="stylesheet">
+    <link href="../assets/css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -51,6 +52,7 @@ var_dump($areas);
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
   </head>
   <body>
   <nav class="navbar navbar-default navbar-fixed-top">
@@ -78,26 +80,48 @@ var_dump($areas);
   <div class="container">
     <div class="row">
       <div class="col-md-4 content-margin-top">
-      <legend>都道府県一覧</legend>
-        <table class="table table-striped table-bordered table-hover table-condensed">
+      <legend><?php echo $area_name['area_name']; ?></legend>
+      <div class="well">男性：2名　女性：1名</div>
+        <table class="table table-striped table-hover table-condensed">
           <thead>
             <tr>
-              <th><div class="text-center">id</div></th>
-              <th><div class="text-center">県名</div></th>
-              <th><div class="text-center">人数</div></th>
+              <th><div class="text-center">名前</div></th>
+              <th><div class="text-center"></div></th>
             </tr>
           </thead>
           <tbody>
-            <!-- id, 県名を表示 -->
-            <?php foreach ($areas as $area) :?>
+            <!-- 友達の名前を表示 -->
             <tr>
-              <td><div class="text-center"><?php echo $area['area_id']; ?></div></td>
-              <td><div class="text-center"><a href="show.php?area_id=<?php echo $area['area_id']; ?>"><?php echo $area['area_name']; ?></a></div></td>
-              <td><div class="text-center">3</div></td>
+              <td><div class="text-center">山田　太郎</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
             </tr>
-          <?php endforeach; ?>
+            <tr>
+              <td><div class="text-center">小林　花子</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td><div class="text-center">佐藤　健</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
+
+        <input type="button" class="btn btn-default" value="新規作成" onClick="location.href='new.html'">
       </div>
     </div>
   </div>
--- show.php
+++ show.php
@@ -1,34 +1,3 @@
-<?php 
-//ここにDBに登録する処理を記述する。
-//１．DBへ接続
-  $dsn = 'mysql:dbname=myfriends;host=localhost';
-  $user = 'root';
-  $password = '';
-$dbh = new PDO($dsn, $user, $password);
-$dbh->query('SET NAMES utf8');
-//2.SQLの作成
-$sql='SELECT * FROM `areas`';
-//3.SQL実行
-$stmt=$dbh->prepare($sql);
-$stmt->execute();
-
-//データ格納用変数
-$areas=array();
-
-//４．データ取得
-while (1) {
-  $rec=$stmt->fetch(PDO::FETCH_ASSOC);
-  if($rec==false){
-    break;
-  }
-  $areas[]=$rec;
-}
-
-//DB切断
-$dbh=null;
-var_dump($areas);
-
- ?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -39,11 +8,11 @@ var_dump($areas);
     <title>myFriends</title>
 
     <!-- Bootstrap -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="assets/css/form.css" rel="stylesheet">
-    <link href="assets/css/timeline.css" rel="stylesheet">
-    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="../assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../assets/css/form.css" rel="stylesheet">
+    <link href="../assets/css/timeline.css" rel="stylesheet">
+    <link href="../assets/css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -51,6 +20,7 @@ var_dump($areas);
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
   </head>
   <body>
   <nav class="navbar navbar-default navbar-fixed-top">
@@ -63,7 +33,7 @@ var_dump($areas);
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="index.php"><span class="strong-title"><i class="fa fa-facebook-square"></i> My friends</span></a>
+              <a class="navbar-brand" href="index.html"><span class="strong-title"><i class="fa fa-facebook-square"></i> My friends</span></a>
           </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -78,26 +48,48 @@ var_dump($areas);
   <div class="container">
     <div class="row">
       <div class="col-md-4 content-margin-top">
-      <legend>都道府県一覧</legend>
-        <table class="table table-striped table-bordered table-hover table-condensed">
+      <legend>北海道の友達</legend>
+      <div class="well">男性：2名　女性：1名</div>
+        <table class="table table-striped table-hover table-condensed">
           <thead>
             <tr>
-              <th><div class="text-center">id</div></th>
-              <th><div class="text-center">県名</div></th>
-              <th><div class="text-center">人数</div></th>
+              <th><div class="text-center">名前</div></th>
+              <th><div class="text-center"></div></th>
             </tr>
           </thead>
           <tbody>
-            <!-- id, 県名を表示 -->
-            <?php foreach ($areas as $area) :?>
+            <!-- 友達の名前を表示 -->
             <tr>
-              <td><div class="text-center"><?php echo $area['area_id']; ?></div></td>
-              <td><div class="text-center"><a href="show.php?area_id=<?php echo $area['area_id']; ?>"><?php echo $area['area_name']; ?></a></div></td>
-              <td><div class="text-center">3</div></td>
+              <td><div class="text-center">山田　太郎</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
             </tr>
-          <?php endforeach; ?>
+            <tr>
+              <td><div class="text-center">小林　花子</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td><div class="text-center">佐藤　健</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
+
+        <input type="button" class="btn btn-default" value="新規作成" onClick="location.href='new.html'">
       </div>
     </div>
   </div>
